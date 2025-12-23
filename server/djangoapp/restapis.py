a08b7780787bff9b2f02673bf20bf7ce015a9bfc
BAHRAOUI MOHAMMED <bahraouimed86@gmail.com>
2025-12-23 18:29:59 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -4,38 +4,28 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    "backend_url",
-    default="http://localhost:3030",
-)
-sentiment_analyzer_url = os.getenv(
-    "sentiment_analyzer_url",
-    default="http://localhost:5050/",
-)
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
+sentiment_analyzer_url = os.getenv("sentiment_analyzer_url", default="http://localhost:5050/")
 
 
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
         for key, value in kwargs.items():
-            params += f"{key}={value}&"
+            params += key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
-
-    print(f"GET from {request_url}")
+    print("GET from {} ".format(request_url))
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as e:
-        # If any error occurs
-        print(f"Network exception occurred: {e}")
+    except Exception as err:
+        print(f"Network exception occurred: {err}")
 
 
 def analyze_review_sentiments(text):
     request_url = sentiment_analyzer_url + "analyze/" + text
     try:
-        # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
     except Exception as err:
@@ -47,7 +37,6 @@ def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
-        print(response.json())
         return response.json()
-    except Exception as e:
-        print(f"Network exception occurred: {e}")
+    except Exception as err:
+        print(f"Network exception occurred: {err}")
