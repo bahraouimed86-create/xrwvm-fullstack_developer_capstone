c9e88c9608c4024beca8b553ab82bf3001282289
BAHRAOUI MOHAMMED <bahraouimed86@gmail.com>
2025-12-23 20:33:10 +0100

    Update restapis.py

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -4,15 +4,17 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv("backend_url", default="http://localhost:3030")
-sentiment_analyzer_url = os.getenv("sentiment_analyzer_url", default="http://localhost:5050/")
+backend_url = os.getenv('backend_url', default="http://localhost:3030")
+sentiment_analyzer_url = os.getenv(
+    'sentiment_analyzer_url', default="http://localhost:5050/"
+)
 
 
 def get_request(endpoint, **kwargs):
     params = ""
     if kwargs:
         for key, value in kwargs.items():
-            params += key + "=" + value + "&"
+            params += f"{key}={value}&"
 
     request_url = backend_url + endpoint + "?" + params
     print("GET from {} ".format(request_url))
@@ -37,6 +39,7 @@ def post_review(data_dict):
     request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
+        print(response.json())
         return response.json()
     except Exception as err:
         print(f"Network exception occurred: {err}")
