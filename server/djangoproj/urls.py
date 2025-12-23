2f806cbc26579f98254976d6e66b01e4ef737cc4
BAHRAOUI MOHAMMED <bahraouimed86@gmail.com>
2025-12-23 18:31:49 +0100

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,10 +1,3 @@
-"""
-djangoproj URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-"""
-
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -20,13 +13,6 @@ urlpatterns = [
     path("login/", TemplateView.as_view(template_name="index.html")),
     path("register/", TemplateView.as_view(template_name="index.html")),
     path("dealers/", TemplateView.as_view(template_name="index.html")),
-    path(
-        "dealer/<int:dealer_id>",
-        TemplateView.as_view(template_name="index.html"),
-    ),
-    path(
-        "postreview/<int:dealer_id>",
-        TemplateView.as_view(template_name="index.html"),
-    ),
+    path("dealer/<int:dealer_id>", TemplateView.as_view(template_name="index.html")),
+    path("postreview/<int:dealer_id>", TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
